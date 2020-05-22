3e6d34e5d0d5e76d1c9ebf33393d7cbecc69d508
Varun Mehta <varun.sagi20@gmail.com>
2020-05-22 14:54:50 +0530

    babel & jest configuration moved to respective files

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	roots: ['<rootDir>/src'],
+	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+	coveragePathIgnorePatterns: [
+		'/node_modules/',
+		'/coverage/',
+		'/logs/',
+		'/.vscode/'
+	],
+	coverageThreshold: {},
+	moduleDirectories: ['node_modules'],
+	transform: { '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest' },
+	transformIgnorePatterns: [
+		'[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
+		'^.+\\.module\\.(css|sass|scss)$'
+	]
+}
