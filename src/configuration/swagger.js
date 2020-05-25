fb3f40464b8737b01062b9dd6c46b6ec033ac1f8
Varun Mehta <varun.sagi20@gmail.com>
2020-05-26 00:00:29 +0530

    swagger basepath fix

--- src/configuration/swagger.js
+++ src/configuration/swagger.js
@@ -5,30 +5,32 @@ import { Router } from 'express'
 /* Configure Swagger for non-production environment */
 
 export default () => {
-	const swaggerDefinition = {
-		openapi: '3.0.3',
-		info: {
-			title: 'Test API',
-			version: '1.0.0',
-			contact: {
-				name: 'Support',
-				email: 'dev@test.com'
-			}
-		},
-		basePath: '/api'
-	}
-
 	const swaggerOptions = {
-		swaggerDefinition,
+		swaggerDefinition: {
+			// openapi: '3.0.3',
+			info: {
+				title: 'Test API',
+				version: '1.0.0',
+				contact: {
+					name: 'Support',
+					email: 'dev@test.com'
+				}
+			},
+			basePath: '/api'
+		},
 		apis: ['**/*.routes.js']
 	}
 
 	const router = Router()
 	const swaggerDocs = swaggerJSDoc(swaggerOptions)
 	const swaggerUrl = '/api/docs'
+	const swaggerUiOptions = {
+		customSiteTitle: 'Stockal Cash Mangement SwaggerUI',
+		customCss: '.swagger-ui .topbar { display: none }' // hide swagger header
+	}
 
 	router.use(swaggerUrl, swaggerUi.serve)
-	router.get(swaggerUrl, swaggerUi.setup(swaggerDocs))
+	router.get(swaggerUrl, swaggerUi.setup(swaggerDocs, swaggerUiOptions))
 	router.get(`${swaggerUrl}/configuration`, (_, res) => {
 		res.status(200).json(swaggerDocs)
 	})
