8210ebf9c39d09d254b835b70595522ac42e84d7
Varun Mehta <varun.sagi20@gmail.com>
2020-06-06 17:05:37 +0530

    axios updated to manage all response error scenarios

--- src/configuration/axios.config.js
+++ src/configuration/axios.config.js
@@ -4,51 +4,108 @@ import { createWinstonLogger } from '../util'
 const logger = createWinstonLogger('axios')
 
 axios.defaults.baseURL = process.env.ICM_URL || 'http://testUrl/api'
-
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 
 logger.debug({ msg: 'config', config: axios.defaults })
 
+const logAxiosResponseError = (error, info) => {
+	const {
+		message,
+		stack,
+		config: { url, method, data }
+	} = error
+
+	logger.error({
+		loggedAt: 'axios response interceptor',
+		status: 'error',
+		method,
+		msg: message,
+		info,
+		url,
+		data,
+		stack
+	})
+}
+
 axios.interceptors.request.use(
 	config => {
+		const { method, data, headers, url } = config
 		logger.info({
-			msg: 'request',
+			loggedAt: 'axios request interceptor',
 			status: 'success',
-			method: config.method,
-			url: config.url,
-			data: config.data,
-			headers: config.headers
+			method,
+			url,
+			data,
+			headers
 		})
 		return config
 	},
 	error => {
-		logger.error({ msg: 'request', status: 'error', error })
+		logger.error({
+			loggedAt: 'axios request interceptor',
+			status: 'error',
+			error
+		})
 		return Promise.reject(error)
 	}
 )
 
 axios.interceptors.response.use(
 	response => {
+		const {
+			config: { url, headers },
+			data,
+			status
+		} = response
 		logger.info({
-			msg: 'response',
+			loggedAt: 'axios response interceptor',
 			status: 'success',
-			statusCode: response.status,
-			url: response.config.url,
-			data: response.data,
-			headers: response.headers
+			statusCode: status,
+			url,
+			data,
+			headers
 		})
 		return response
 	},
 	error => {
-		const { status, statusText, config, data } = error.response
-		logger.error({
-			status: 'error',
-			statusCode: status,
-			method: config.method,
-			msg: statusText,
-			url: config.url,
-			data
-		})
-		return Promise.reject(error.response)
+		if (error.response) {
+			// The request was made and the server responded with a status code
+			// that falls out of the range of 2xx
+			const {
+				status,
+				statusText,
+				config: { url, method },
+				data
+			} = error.response
+
+			logger.error({
+				loggedAt: 'axios response interceptor',
+				status: 'error',
+				statusCode: status,
+				statusText,
+				method,
+				msg: error.message,
+				url,
+				data
+			})
+
+			return Promise.reject(error.response)
+		} else if (error.request) {
+			// The request was made but no response was received
+			logAxiosResponseError(error, 'No response received for axios request')
+
+			// Hiding system error message
+			error.status = 503
+			error.message = 'Service Unavailable'
+			return Promise.reject(error)
+		} else {
+			// Something happened in setting up the request that triggered an Error
+			logAxiosResponseError(error, 'No request made by axios')
+
+			// Hiding system error message
+			error.status = 503
+			error.message = 'Service Unavailable'
+			return Promise.reject(error)
+		}
 	}
 )
